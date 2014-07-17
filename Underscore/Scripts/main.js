b1b82a0ea38d7fd541350b6f76932e93df92340f
Dimitar Mitev <dnmitev@gmail.com>
2014-07-17 12:26:31 +0300

    underscore hw starter :fist:

---
+++ Underscore/Scripts/main.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="_references.js" />
+(function () {
+    'use strict';
+
+    require.config({
+        paths: {
+            underscore: 'libs/underscore',
+            jquery: 'libs/jquery-2.1.1',
+            hb: 'libs/handlebars',
+            st: 'students',
+            an: 'animals',
+            b: 'books'
+        }
+    });
+
+    require(['underscore', 'st', 'template'], function (_, students) {
+        var sortedByFirstName = _.filter(students, function (st) {
+            return st.firstName < st.lastName;
+        });
+
+        console.dir(sortedByFirstName);
+
+        $('#students-list').template(sortedByFirstName);
+
+    });
+}());
---
+++ Underscore/Scripts/students.js
@@ -0,0 +1,174 @@
+﻿/// <reference path="_references.js" />
+define(function () {
+    "use strict";
+
+    var students = [{
+        firstName: 'Ivan',
+        lastName: 'Draganov',
+        age: 17,
+        marks: {
+            math: 6,
+            physics: 6,
+            biology: 5,
+            history: 3,
+            science: 4
+        }
+    }, {
+        firstName: 'Ivan',
+        lastName: 'Petkanov',
+        age: 27,
+        marks: {
+            math: 4,
+            physics: 2,
+            biology: 3,
+            history: 3,
+            science: 4
+        }
+    }, {
+        firstName: 'Ivan',
+        lastName: 'Georgiev',
+        age: 17,
+        marks: {
+            math: 4,
+            physics: 2,
+            biology: 2,
+            history: 2,
+            science: 3
+        }
+    }, {
+        firstName: 'Georgi',
+        lastName: 'Goshov',
+        age: 18,
+        marks: {
+            math: 4,
+            physics: 4,
+            biology: 4,
+            history: 4,
+            science: 4
+        }
+    }, {
+        firstName: 'Petar',
+        lastName: 'Pichakov',
+        age: 19,
+        marks: {
+            math: 6,
+            physics: 6,
+            biology: 6,
+            history: 6,
+            science: 6
+        }
+    }, {
+        firstName: 'Petar',
+        lastName: 'Peshov',
+        age: 21,
+        marks: {
+            math: 3,
+            physics: 3,
+            biology: 2,
+            history: 3,
+            science: 4
+        }
+    }, {
+        firstName: 'Pesho',
+        lastName: 'Peshev',
+        age: 23,
+        marks: {
+            math: 3,
+            physics: 3,
+            biology: 3,
+            history: 3,
+            science: 3
+        }
+    }, {
+        firstName: 'Dimitar',
+        lastName: 'Penev',
+        age: 23,
+        marks: {
+            math: 2,
+            physics: 2,
+            biology: 2,
+            history: 2,
+            science: 2
+        }
+    }, {
+        firstName: 'Dimitar',
+        lastName: 'Paynera',
+        age: 56,
+        marks: {
+            math: 2,
+            physics: 2,
+            biology: 2,
+            history: 3,
+            science: 2
+        }
+    }, {
+        firstName: 'Petra',
+        lastName: 'HishtnaHiena',
+        age: 22,
+        marks: {
+            math: 3,
+            physics: 6,
+            biology: 4,
+            history: 3,
+            science: 4
+        }
+    }, {
+        firstName: 'Bat',
+        lastName: 'Giorgi',
+        age: 20,
+        marks: {
+            math: 6,
+            physics: 6,
+            biology: 5,
+            history: 3,
+            science: 4
+        }
+    }, {
+        firstName: 'Dinko',
+        lastName: 'Zadushaamsa',
+        age: 17,
+        marks: {
+            math: 6,
+            physics: 6,
+            biology: 6,
+            history: 6,
+            science: 6
+        }
+    }, {
+        firstName: 'Spindi',
+        lastName: 'Spindermanov',
+        age: 20,
+        marks: {
+            math: 4,
+            physics: 4,
+            biology: 5,
+            history: 4,
+            science: 5
+        }
+    }, {
+        firstName: 'Sashka',
+        lastName: 'Vaseva',
+        age: 107,
+        marks: {
+            math: 2,
+            physics: 2,
+            biology: 2,
+            history: 2,
+            science: 2
+        }
+    }, {
+        firstName: 'Zoro',
+        lastName: 'Nazorov',
+        age: 27,
+        marks: {
+            math: 6,
+            physics: 6,
+            biology: 6,
+            history: 2,
+            science: 2
+        }
+    }
+    ];
+
+    return students;
+})
